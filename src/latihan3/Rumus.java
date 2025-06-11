cd322def12960358a77556ac3c67f0f5ac98285a
efraim urel <efraimurelpalodang@gmail.com>
2025-06-11 15:52:03 +0800

    membuat method

--- src/latihan3/Rumus.java
+++ src/latihan3/Rumus.java
@@ -6,4 +6,17 @@ package latihan3;
 
 public class Rumus {
     int sisi, panjang, lebar, hasil;
+    
+    public int hitungLuasPersegi(int sisi) {
+        this.sisi = sisi;
+        this.hasil = sisi * sisi;
+        return this.hasil;
+    }
+    
+    public int hitungLuasPersegiPanjang(int panjang, int lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.hasil = panjang * lebar;
+        return this.hasil;
+    }
 }
